2c2fff906776942103c1dbfa47b748250829cc45
Janay Bailey <tjanay87@gmail.com>
2019-09-30 21:38:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,19 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
- GENRES = []
+ GENRES = [describe 'GENRES' do
+  it 'keeps track of all genres' do
+    genres = ["Thriller", "Science Fiction", "Romance"]
+    genres.each_with_index do |genre, i|
+      book = Book.new("Book_#{i}")
+      book.genre = genre
+    end
+ 
+    genres.each do |genre|
+      expect(Book::GENRES).to include(genre)
+    end
+  end
+end]
   def initialize(title)
     @title = title
   end
